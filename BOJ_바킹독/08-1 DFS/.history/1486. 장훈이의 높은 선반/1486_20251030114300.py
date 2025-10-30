27dd230c92e8fd850c536b41e0581d12c1538a9e
대전4반이선영 <zlzlakfls@naver.com>
2025-10-30 13:06:37 +0900

    장훈이

---
+++ BOJ_바킹독/08-1 DFS/.history/1486. 장훈이의 높은 선반/1486_20251030114300.py
@@ -0,0 +1,6 @@
+tc = int(input())
+
+n, b = map(int, input().split())
+heights = list(int(input().split()))
+
+print(heights)
---
+++ BOJ_바킹독/08-1 DFS/.history/1486. 장훈이의 높은 선반/1486_20251030114315.py
@@ -0,0 +1,6 @@
+tc = int(input())
+
+n, b = map(int, input().split())
+heights = [int(input().split())]
+
+print(heights)
